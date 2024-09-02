192ad5ec679771badb7020cb1e177790e24c2e0f
Jay/Fienna Liang <jay.liang@bluextrade.com>
2024-09-02 17:51:30 +0800

    Use Milvus as vector database

--- app/api/chat/engine/index.ts
+++ app/api/chat/engine/index.ts
@@ -1,5 +1,6 @@
 import { VectorStoreIndex } from "llamaindex";
 import { PGVectorStore } from "llamaindex/storage/vectorStore/PGVectorStore";
+import { MilvusVectorStore } from "llamaindex/storage/vectorStore/MilvusVectorStore";
 import {
   PGVECTOR_SCHEMA,
   PGVECTOR_TABLE,
@@ -7,11 +8,19 @@ import {
 } from "./shared";
 
 export async function getDataSource() {
-  checkRequiredEnvVars();
-  const pgvs = new PGVectorStore({
-    connectionString: process.env.PG_CONNECTION_STRING,
-    schemaName: PGVECTOR_SCHEMA,
-    tableName: PGVECTOR_TABLE,
+  // checkRequiredEnvVars();
+  // const pgvs = new PGVectorStore({
+  //   connectionString: process.env.PG_CONNECTION_STRING,
+  //   schemaName: PGVECTOR_SCHEMA,
+  //   tableName: PGVECTOR_TABLE,
+  // });
+  // return await VectorStoreIndex.fromVectorStore(pgvs);
+  const milvusvs = new MilvusVectorStore({
+    collection: "embedding_1536",
+    embeddingKey: "embeddings",
+    idKey: "id",
+    contentKey: "text",
+    metadataKey: '$meta'
   });
-  return await VectorStoreIndex.fromVectorStore(pgvs);
+  return await VectorStoreIndex.fromVectorStore(milvusvs);
 }
